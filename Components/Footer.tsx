23b3b360cb3e2202e3f6bad1b345dc2190b69435
Arief-alt <145629626+Arief-alt@users.noreply.github.com>
2025-06-24 09:57:19 +0700

    Add Navbar +Footer

---
+++ Components/Footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <nav className="w-full p-6 fixed glassmorphism border-t border-gray-500">
+            <header className="wrapper justify-center flex flex-col gap-3">
+                <div className="flex items-center gap-3">
+                    <div className="w-12 h-12 rounded-full flex items-center justify-center bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 shadow-md"/>
+
+                    <h1 className="title-gradient-text text-2xl font-bold">
+                        Studio Ghibli Collection
+                    </h1>
+                </div>
+
+                <span className="text-gray-400">
+                    Celebrating the magical storytelling and artistry of Hayao Miyazaki and Studio Ghibli.
+                </span>
+
+                <span className="text-gray-500 text-xs mt-2">
+                    &copy; {new Date().getFullYear()} Ghibli Studio Fan App. All rights reserved.
+                </span>
+            </header>
+        </nav>
+    )
+}
+export default Footer
---
+++ Components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="w-full p-6 fixed glassmorphism border-b border-gray-500">
+            <header className="wrapper justify-center flex">
+                <div className="flex items-center gap-3">
+                    <div className="w-12 h-12 rounded-full flex items-center justify-center bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 shadow-md">
+                        <span className="font-serif text-white text-2xl font-bold">G</span>
+                    </div>
+
+                    <h1 className="title-gradient-text text-2xl font-bold">
+                        Ghibli Studio
+                    </h1>
+                </div>
+            </header>
+        </nav>
+    )
+}
+export default Navbar
