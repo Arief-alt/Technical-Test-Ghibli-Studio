64aecffe7e82836cec2ca61d83111bcbbc34585e
Arief-alt <145629626+Arief-alt@users.noreply.github.com>
2025-06-24 15:36:23 +0700

    Fix UI position

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <nav className="w-full p-6 glassmorphism border-t border-gray-500">
             <header className="wrapper justify-center flex flex-col gap-3">
-                <div className="flex items-center gap-3">
+                <div className="flex justify-center items-center gap-3">
                     <div className="w-12 h-12 rounded-full flex items-center justify-center bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 shadow-md"/>
 
                     <h1 className="title-gradient-text text-2xl font-bold">
@@ -16,7 +16,7 @@ const Footer = () => {
                     Celebrating the magical storytelling and artistry of Hayao Miyazaki and Studio Ghibli.
                 </span>
 
-                <span className="text-gray-500 text-xs mt-2">
+                <span className="text-gray-500 text-xs mt-2 text-center">
                     &copy; {new Date().getFullYear()} Ghibli Studio Fan App. All rights reserved.
                 </span>
             </header>
